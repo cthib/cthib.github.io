8ca8c87d6c1a09dd0d865c7ff2b02598b37d8450
cthib <cortland.thibodeau@gmail.com>
2018-01-26 09:59:47 -0800

    2018 updates, turning into web resume

---
+++ js/site.js
@@ -0,0 +1,6 @@
+var $target = $('body');
+inView.offset(500);
+inView('.change-bg').on('enter', function(el){
+  var url = $(el).attr('data-background-url');
+  $target.css('background-image', 'url("' + url + '")');
+});
